40bffd340d2011df666aaf908c7f81c2ce19f675
mnyaxx <myranyakiamo11@gmail.com>
2024-07-29 14:57:52 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: 'Myra Donnavan',
+        bio: 'Software Developer',
+        imgSrc: 'https://toppng.com/uploads/preview/erson-outline-icon-png-person-icon-png-white-11562864385wyirbbsupu.png',
+        profession: 'React Developer'
+      },
+      shows: false,
+      timeInterval: 0
+    };
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  // Toggle show state
+  handleToggle() {
+    this.setState(prevState => ({ shows: !prevState.shows }));
+  }
+
+  // Set up timer on component mount
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  // Clear timer on component unmount
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  // Update time interval
+  tick() {
+    this.setState(prevState => ({ timeInterval: prevState.timeInterval + 1 }));
+  }
+
+  render() {
+    const { person, shows, timeInterval } = this.state;
+    return (
+      <div className="App">
+        <button onClick={this.handleToggle}>
+          {shows ? 'Hide Profile' : 'Show Profile'}
+        </button>
+        {shows && (
+          <div className="profile">
+            <img src={person.imgSrc} alt={person.fullName} />
+            <h2>{person.fullName}</h2>
+            <p>{person.bio}</p>
+            <p>{person.profession}</p>
+          </div>
+        )}
+        <div>Time since mount: {timeInterval} seconds</div>
+      </div>
+    );
+  }
 }
 
 export default App;
